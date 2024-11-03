b41878681e008dc414d950b040cc032de21d3c7f
apykhanov <121295115+apykhanov@users.noreply.github.com>
2024-11-03 18:43:04 +0300

    Отрисуй меня полностью

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,18 +1,24 @@
 import {getPhotoArray} from './data.js';
 
 const template = document.querySelector('#picture').content.querySelector('.picture');
-const fragment = document.createDocumentFragment();
-
-getPhotoArray.forEach((photo) => {
+const container = document.querySelector('.picture');
+const createThumbnail = (photo) => {
   const thumbnail = template.cloneNode(true);
-  const image = document.querySelector('.picture__img');
+  const image = thumbnail.querySelector('.picture__img');
   image.src = photo.url;
   image.alt = photo.description;
 
   thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
   thumbnail.querySelector('.picture__likes').textContent = photo.likes;
 
+  return thumbnail;
+};
+const fragment = document.createDocumentFragment();
+getPhotoArray.forEach((photo) => {
+  const thumbnail = createThumbnail(photo);
+
   fragment.appendChild(thumbnail);
 
 });
+container.appendChild(fragment);
 
