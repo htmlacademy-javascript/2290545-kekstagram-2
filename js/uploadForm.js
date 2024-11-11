1c7ac9699a448123a246b68d43f9816f13eb2e6a
apykhanov <121295115+apykhanov@users.noreply.github.com>
2024-11-11 19:28:43 +0300

    Правда или действие

--- js/uploadForm.js
+++ js/uploadForm.js
@@ -8,22 +8,39 @@ const editorReset = editorForm.querySelector('.img-upload__cancel');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
 
+const CLASSES = {
+  HIDDEN: 'hidden',
+  MODAL_OPEN: 'modal-open',
+  ERROR: 'img-upload__field-wrapper--error'
+};
+
+const VALIDATION_RULES = {
+  HASHTAG_PATTERN: /^#[a-zа-яё0-9]{1,19}$/i,
+  MAX_COMMENT_LENGTH: 140
+};
+
+const PRISTINE_CONFIG = {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: CLASSES.ERROR,
+  errorTextParent: 'img-upload__field-wrapper'
+};
+
 const onEditorReset = () => closeEditor();
 
-const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)
-    && !evt.target.classList.contains('text__hashtags')
-    && !evt.target.classList.contains('text__description')
-  ) {
-    evt.preventDefault();
+const shouldCloseEditor = (evt) => isEscapeKey(evt) &&
+    !evt.target.classList.contains('text__hashtags') &&
+    !evt.target.classList.contains('text__description');
 
+const onDocumentKeydown = (evt) => {
+  if (shouldCloseEditor(evt)) {
+    evt.preventDefault();
     closeEditor();
   }
 };
 
 function closeEditor() {
-  editorForm.classList.add('hidden');
-  pageBody.classList.remove('modal-open');
+  editorForm.classList.add(CLASSES.HIDDEN);
+  pageBody.classList.remove(CLASSES.MODAL_OPEN);
   document.removeEventListener('keydown', onDocumentKeydown);
   editorReset.removeEventListener('click', onEditorReset);
   uploadFile.value = '';
@@ -31,19 +48,15 @@ function closeEditor() {
 
 export const initUploadModal = () => {
   uploadFile.addEventListener('change', () => {
-    editorForm.classList.remove('hidden');
-    pageBody.classList.add('modal-open');
+    editorForm.classList.remove(CLASSES.HIDDEN);
+    pageBody.classList.add(CLASSES.MODAL_OPEN);
     editorReset.addEventListener('click', onEditorReset);
   });
 };
 
-const pristine = new Pristine(form, {
-  classTo: 'img-upload__field-wrapper',
-  errorClass: 'img-upload__field-wrapper--error',
-  errorTextParent: 'img-upload__field-wrapper',
-});
+const pristine = new Pristine(form, {PRISTINE_CONFIG});
 
-pristine.addValidator(hashtagInput, (value) =>/^#[a-zа-яё0-9]{1,19}$/i.test(value));
+pristine.addValidator(hashtagInput, (value) =>VALIDATION_RULES.HASHTAG_PATTERN.test(value));
 
-pristine.addValidator(commentInput, (value) => value.length <= 140);
+pristine.addValidator(commentInput, (value) => value.length <= VALIDATION_RULES.MAX_COMMENT_LENGTH);
 
